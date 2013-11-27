db8e080b61eb36d02d2d309a346a9318fb3124e8
george <algoritmo.linux@gmail.com>
2013-11-27 17:52:19 -0200

    commentarios em Fatorial.java atualizados

--- Fatorial.java
+++ Fatorial.java
@@ -1,5 +1,5 @@
 public class Fatorial{
-	//versao interativa do calculo do fatorial
+	//versao interativa do calculo do fatorial, incluindo o teste para n iguall a zero
 	public int calcularFatorial(int n){
 		if(n < 0)
 			throw new IllegalArgumentException("nao exeite fatorial de numero negativo");
