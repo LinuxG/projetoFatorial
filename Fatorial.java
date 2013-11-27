e3d904bfb1b874f41d4ffdb14e9b21af96130179
george <algoritmo.linux@gmail.com>
2013-11-27 14:11:34 -0200

    alteracao para lancar excecao em caso de numeros negativos

--- Fatorial.java
+++ Fatorial.java
@@ -1,6 +1,8 @@
 public class Fatorial{
 	//versao interativa do calculo do fatorial
 	public int calcularFatorial(int n){
+		if(n < 0)
+			throw new IllegalArgumentException("nao exeite fatorial de numero negativo");
 		int resultado = 1;
 
 		for(int i = 2; i <= n;i++)
