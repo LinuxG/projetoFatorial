364124ef5b4c04e0332b25732664c46e55beef87
george <algoritmo.linux@gmail.com>
2013-11-27 17:54:11 -0200

    comentario inserido em TesteFatorial.java

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -1,4 +1,5 @@
 public class TesteFatorial{
+	//comentario inserido em TEsteFatorioal.java
 	public static void main(String args[]){
 		Fatorial fat = new Fatorial();
 		int resultado = fat.calcularFatorial(5);
